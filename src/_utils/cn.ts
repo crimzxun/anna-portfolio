d75ff4ad9d532d87562c4b32108632bbf80174ba
Anna Zheng <zanna3715@gmail.com>
2025-09-08 21:50:54 -0400

    update resume + commented cn.ts

--- src/_utils/cn.ts
+++ src/_utils/cn.ts
@@ -1,7 +1,7 @@
-import { ClassValue, clsx } from 'clsx';    // allows to conditionally join class names
-import { twMerge } from 'tailwind-merge';   // resolves class conflicts by keeping only the last one
+// import { ClassValue, clsx } from 'clsx';    // allows to conditionally join class names
+// import { twMerge } from 'tailwind-merge';   // resolves class conflicts by keeping only the last one
 
-export const cn = (...inputs: ClassValue[]) => {
-    // allows conditional classes and resolves Tailwind conflicts in one go
-    return twMerge(clsx(inputs));
-}
+// export const cn = (...inputs: ClassValue[]) => {
+//     // allows conditional classes and resolves Tailwind conflicts in one go
+//     return twMerge(clsx(inputs));
+// }
